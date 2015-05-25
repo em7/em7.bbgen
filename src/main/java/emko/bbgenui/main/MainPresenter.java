65f890e7abc28c401f08a80fa8b0bd8dc20c97f1
eMko <mm007.emko@gmail.com>
2015-05-25 11:00:21 +0200

    Added transitions for switching scenes

--- src/main/java/emko/bbgenui/main/MainPresenter.java
+++ src/main/java/emko/bbgenui/main/MainPresenter.java
@@ -5,7 +5,9 @@ import java.util.ResourceBundle;
 
 import emko.bbgenui.board.BoardView;
 import emko.bbgenui.wordlist.WordListView;
+import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
+import javafx.event.EventHandler;
 import javafx.event.EventType;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
@@ -19,6 +21,7 @@ import javafx.stage.WindowEvent;
 import javafx.scene.control.Button;
 import javafx.util.Duration;
 import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
 
 public class MainPresenter implements Initializable {
 
@@ -26,26 +29,27 @@ public class MainPresenter implements Initializable {
 	//@FXML AnchorPane subMainPane;
 	
 	WordListView wordListView;
+	Parent wordListViewParent;
 	
 	BoardView boardView;
+	Parent boardViewParent;
 
-	@FXML Button boardBtn;
+	@FXML Label screenNameLabel;
 
-	@FXML Button wordListBtn;
+
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		double em = Font.getDefault().getSize();
-		
-//		wordListView = new WordListView();
-//		Parent wordListViewParent = wordListView.getView();
-//		double em = Font.getDefault().getSize();
-//		AnchorPane.setLeftAnchor(wordListViewParent, 0.5 * em);
-//		AnchorPane.setRightAnchor(wordListViewParent, 0.5 * em);
-//		AnchorPane.setTopAnchor(wordListViewParent, 0.5 * em);
-//		AnchorPane.setBottomAnchor(wordListViewParent, 0.5 * em);
-//
-//		mainPane.getChildren().addAll(wordListViewParent);
+				
+		//create word list view
+		wordListView = new WordListView();
+		Parent wordListViewParent = wordListView.getView();
+		AnchorPane.setLeftAnchor(wordListViewParent, 0.5 * em);
+		AnchorPane.setRightAnchor(wordListViewParent, 0.5 * em);
+		AnchorPane.setTopAnchor(wordListViewParent, 0.5 * em);
+		AnchorPane.setBottomAnchor(wordListViewParent, 0.5 * em);
+		this.wordListViewParent = wordListViewParent;
 		
 		//create a board view
 		boardView = new BoardView();
@@ -54,54 +58,79 @@ public class MainPresenter implements Initializable {
 		AnchorPane.setRightAnchor(boardViewParent, 0.5 * em);
 		AnchorPane.setTopAnchor(boardViewParent, 0.5 * em);
 		AnchorPane.setBottomAnchor(boardViewParent, 0.5 * em);
+		this.boardViewParent = boardViewParent;
 
-		//add to graph
+		//add default view to graph
 		mainPane.getChildren().add(boardViewParent);		
-
-		
-		
-		
-		
-		//wordListView.getPresenter();
 	}
 
-	@FXML public void showBoard(ActionEvent event) {
-		
+	@FXML public void showBoard() {
+		swipeContent(SwipeDirection.RIGHT, boardViewParent);
+		changeScreenName("Board");
+	}
+	
+	@FXML public void showWordList() {
+		swipeContent(SwipeDirection.LEFT, wordListViewParent);
+		changeScreenName("Words");
 	}
 
-	@FXML public void showWordList(ActionEvent event) {
+	private enum SwipeDirection {
+		LEFT, RIGHT
+	}
+	
+	private void swipeContent(SwipeDirection direction, Parent newContent) {
 		//the animation boundary
-		double translateX = mainPane.getWidth() + 10; //pane width + reserve
+		double transX = mainPane.getWidth() + 10; // pane width + reserve
+		if (direction == SwipeDirection.RIGHT)
+			transX = - transX;
 		
-		//move everything out
-		if (mainPane.getChildren().size() > 0) {
-			for (Node child : mainPane.getChildren()) {
-				TranslateTransition moveLeftTra = new TranslateTransition(Duration.millis(500d), child);
-				moveLeftTra.setByX(- translateX);
-				moveLeftTra.play();				
-			}
-		}
-		
-		//clear the graph
-		mainPane.getChildren().clear();
-		
-		//create the word list
-		wordListView = new WordListView();
-		Parent wordListViewParent = wordListView.getView();
-		double em = Font.getDefault().getSize();
-		AnchorPane.setLeftAnchor(wordListViewParent, 0.5 * em);
-		AnchorPane.setRightAnchor(wordListViewParent, 0.5 * em);
-		AnchorPane.setTopAnchor(wordListViewParent, 0.5 * em);
-		AnchorPane.setBottomAnchor(wordListViewParent, 0.5 * em);
+		final double translateX = transX; //it's used from anonymous innter class, must be final
 
-		//move it out of the scene
-		wordListViewParent.setTranslateX(translateX);		
-		mainPane.getChildren().addAll(wordListViewParent);
+		TranslateTransition moveOutTra = new TranslateTransition(Duration.millis(250d), mainPane);
+		moveOutTra.setByX(-translateX);
 		
-		//animate it back
-		TranslateTransition moveLeftTra = new TranslateTransition(Duration.millis(500), wordListViewParent);
-		moveLeftTra.setByX(- translateX);
-		moveLeftTra.play();
+		FadeTransition fadeOutTra = new FadeTransition(Duration.millis(300d), mainPane);
+		fadeOutTra.setFromValue(1.0);
+		fadeOutTra.setToValue(0.3);
+
+		moveOutTra.setOnFinished(evt -> {
+			//clear the graph
+			mainPane.getChildren().clear();
+			
+			//move new content out of the scene
+			mainPane.setTranslateX(translateX);		
+			mainPane.getChildren().addAll(newContent);
+			
+			//animate it back
+			TranslateTransition moveInTra = new TranslateTransition(Duration.millis(250), mainPane);
+			moveInTra.setByX(- translateX);
+			
+			FadeTransition fadeInTra = new FadeTransition(Duration.millis(300d), mainPane);
+			fadeInTra.setFromValue(0.3);
+			fadeInTra.setToValue(1.0);
+			
+			moveInTra.play();
+			fadeInTra.play();
+		});
+		
+		fadeOutTra.play();
+		moveOutTra.play();
+		
+	}
+
+	private void changeScreenName(String newName) {
+		FadeTransition fadeOutTra = new FadeTransition(Duration.millis(250d), screenNameLabel);
+		fadeOutTra.setFromValue(1.0);
+		fadeOutTra.setToValue(0d);
+		fadeOutTra.setOnFinished(evt -> {
+			screenNameLabel.setText(newName);
+			FadeTransition fadeInTra = new FadeTransition(Duration.millis(250d), screenNameLabel);
+			fadeInTra.setFromValue(0d);
+			fadeInTra.setToValue(1.0);
+			fadeInTra.play();
+		});
+		
+		fadeOutTra.play();
 	}
 	
 
